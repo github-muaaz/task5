89ddef74f034aadfc4e598c9d396a3123a7e856d
Muaaz [Muhammadaziz] <muhammadaziz.erg.1229@gmail.com>
2024-05-02 08:36:29 +0500

    ok

--- src/users.jsx
+++ src/users.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import faker from 'faker';
 import { CSVLink } from 'react-csv';
 
